dfee6ec4ff787e12789b3007421f0aaad493a1c7
sabarireddy <sabarireddy.chinna@gmail.com>
2020-06-17 11:59:49 +0530

    Update mountazurefiles.sh

--- mountazurefiles.sh
+++ mountazurefiles.sh
@@ -13,6 +13,7 @@ if [ ! -f "/etc/smbcredentials/prodqaeastusstorage.cred" ]; then
     sudo bash -c 'echo "username="$1"" >> /etc/smbcredentials/prodqaeastusstorage.cred'
     sudo bash -c 'echo "password="$2"" >> /etc/smbcredentials/prodqaeastusstorage.cred'
 fi
+
 sudo chmod 600 /etc/smbcredentials/prodqaeastusstorage.cred
 
 sudo bash -c 'echo "//"$1".file.core.windows.net/"$3" "$4" -o vers=3.0,credentials=/etc/smbcredentials/prodqaeastusstorage.cred,dir_mode=0755,file_mode=0664" >> /etc/fstab'
